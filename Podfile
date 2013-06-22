349c161dca788420c0995f4d823c8b711a15447b
Josh Bassett <josh.bassett@gmail.com>
2013-06-22 14:33:25 +1000

    Added RoutingHTTPServer library.

--- Podfile
+++ Podfile
@@ -1,1 +1,2 @@
 pod 'CocoaHTTPServer'
+pod 'RoutingHTTPServer'
--- flatland/AppDelegate.m
+++ flatland/AppDelegate.m
@@ -7,16 +7,11 @@
 //
 
 #import "AppDelegate.h"
-#import "HTTPServer.h"
-#import "DDLog.h"
-#import "DDTTYLogger.h"
+#import "RoutingHTTPServer.h"
 #import "MyScene.h"
 
-// Log levels: off, error, warn, info, verbose.
-static const int ddLogLevel = LOG_LEVEL_VERBOSE;
-
 @implementation AppDelegate {
-	HTTPServer *httpServer;
+	RoutingHTTPServer *httpServer;
 }
 
 @synthesize window = _window;
@@ -33,31 +28,21 @@ static const int ddLogLevel = LOG_LEVEL_VERBOSE;
   self.skView.showsFPS = YES;
   self.skView.showsNodeCount = YES;
 
-	// Configure our logging framework.
-	// To keep things simple and fast, we're just going to log to the Xcode console.
-	[DDLog addLogger:[DDTTYLogger sharedInstance]];
+  httpServer = [[RoutingHTTPServer alloc] init];
+  [httpServer setPort:8000];
+  [httpServer setDefaultHeader:@"Server" value:@"YourAwesomeApp/1.0"];
 
-	// Initalize our http server
-	httpServer = [[HTTPServer alloc] init];
+	[httpServer get:@"/hello" withBlock:^(RouteRequest *request, RouteResponse *response) {
+		[response respondWithString:@"Hello!"];
+	}];
 
-	// Tell the server to broadcast its presence via Bonjour.
-	// This allows browsers such as Safari to automatically discover our service.
-	[httpServer setType:@"_http._tcp."];
+	[httpServer get:@"/hello/:name" withBlock:^(RouteRequest *request, RouteResponse *response) {
+		[response respondWithString:[NSString stringWithFormat:@"Hello %@!", [request param:@"name"]]];
+	}];
 
-	// Normally there's no need to run our server on any specific port.
-	// Technologies like Bonjour allow clients to dynamically discover the server's port at runtime.
-	// However, for easy testing you may want force a certain port so you can just hit the refresh button.
-  //	[httpServer setPort:12345];
-
-	// Serve files from the standard Sites folder
-	NSString *docRoot = [@"~/Sites" stringByExpandingTildeInPath];
-	DDLogInfo(@"Setting document root: %@", docRoot);
-
-	[httpServer setDocumentRoot:docRoot];
-
-	NSError *error = nil;
-	if(![httpServer start:&error]) {
-		DDLogError(@"Error starting HTTP Server: %@", error);
+  NSError *error;
+	if (![httpServer start:&error]) {
+		NSLog(@"Error starting HTTP server: %@", error);
 	}
 }
 
