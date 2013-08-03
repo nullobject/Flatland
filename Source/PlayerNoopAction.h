2b1c269588451eec04d21e0dbbd202e9dd9339be
Josh Bassett <josh.bassett@gmail.com>
2013-08-03 19:21:15 +1000

    Added noop action.

---
+++ Source/PlayerNoopAction.h
@@ -0,0 +1,12 @@
+//
+//  PlayerNoopAction.h
+//  Flatland
+//
+//  Created by Josh Bassett on 3/08/2013.
+//  Copyright (c) 2013 Gamedogs. All rights reserved.
+//
+
+#import "PlayerAction.h"
+
+@interface PlayerNoopAction : PlayerAction
+@end
---
+++ Source/PlayerNoopAction.m
@@ -0,0 +1,12 @@
+//
+//  PlayerNoopAction.m
+//  Flatland
+//
+//  Created by Josh Bassett on 3/08/2013.
+//  Copyright (c) 2013 Gamedogs. All rights reserved.
+//
+
+#import "PlayerNoopAction.h"
+
+@implementation PlayerNoopAction
+@end
